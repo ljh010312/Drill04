72e107f565e72f4bcd798e0794e1506420a0ea5b
Lee9747 <leejunho32@naver.com>
2023-09-28 18:16:58 +0900

    add up, down move

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -17,9 +17,13 @@ move_right = (
     Sprite(155, 726, 18, 38),
     Sprite(177, 726, 27, 36),
     Sprite(206, 726, 28, 36),
-    Sprite(239, 316, 24, 37)
+    Sprite(239, 726, 24, 37)
 )
 move_idle = (
+    Sprite(32, 874, 35, 37),
+    Sprite(67, 874, 35, 37),
+    Sprite(102, 874, 35, 37),
+    Sprite(137, 874, 35, 37),
     Sprite(32, 874, 35, 37),
     Sprite(67, 874, 35, 37),
     Sprite(102, 874, 35, 37),
@@ -32,7 +36,7 @@ character = load_image('spritesheets.png')
 
 
 def handle_events():
-    global running, h_dir
+    global running, dir
 
     events = get_events()
     for event in events:
@@ -42,37 +46,51 @@ def handle_events():
             if event.key == SDLK_ESCAPE:
                 running = False
             elif event.key == SDLK_RIGHT:
-                h_dir += 1
+                dir = 1
             elif event.key == SDLK_LEFT:
-                h_dir -= 1
+                dir = -1
+            elif event.key == SDLK_UP:
+                dir = 2
+            elif event.key == SDLK_DOWN:
+                dir = -2
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                h_dir -= 1
+                dir = 0
             elif event.key == SDLK_LEFT:
-                h_dir += 1
+                dir = 0
+            elif event.key == SDLK_UP:
+                dir = 0
+            elif event.key == SDLK_DOWN:
+                dir = 0
 def character_move():
-    global running, h_dir, frame, x,y
+    global running, dir, frame, x,y
 
-    if h_dir == 1:
-        character.clip_draw(move_right[frame].x, move_right[frame].y, move_right[frame].w, move_right[frame].h , x, y)
-        frame = (frame + 1) % 8
-        x += h_dir * speed
-        if x > TUK_WIDTH:
+    if dir == 1:
+        character.clip_draw(move_right[frame].x, move_right[frame].y, move_right[frame].w, move_right[frame].h , x, y, move_right[frame].w * 2, move_right[frame].h * 2)
+        x += dir * speed
+        if x > TUK_WIDTH - 20:
             x -= speed
-    elif h_dir == 0:
-        frame = (frame + 1) % 4
-        character.clip_draw(move_idle[frame].x, move_idle[frame].y, move_idle[frame].w, move_idle[frame].h, x, y)
-    elif h_dir == -1:
-        character.clip_composite_draw(move_right[frame].x, move_right[frame].y, move_right[frame].w, move_right[frame].h, 0, 'h', x, y, move_right[frame].w, move_right[frame].h)
-        frame = (frame + 1) % 8
-        x += h_dir * speed
-        if x < 0:
+    elif dir == 0:
+        character.clip_draw(move_idle[frame].x, move_idle[frame].y, move_idle[frame].w, move_idle[frame].h, x, y, move_idle[frame].w * 2, move_idle[frame].h * 2)
+    elif dir == -1:
+        character.clip_composite_draw(move_right[frame].x, move_right[frame].y, move_right[frame].w, move_right[frame].h, 0, 'h', x, y, move_right[frame].w * 2, move_right[frame].h * 2)
+        x += dir * speed
+        if x < 20:
             x += speed
-
-
+    elif dir == 2:
+        character.clip_draw(move_right[frame].x, move_right[frame].y, move_right[frame].w, move_right[frame].h, x, y,move_right[frame].w * 2, move_right[frame].h * 2)
+        y += speed
+        if y > TUK_HEIGHT - 20:
+            y -= speed
+    elif dir == -2:
+        character.clip_draw(move_right[frame].x, move_right[frame].y, move_right[frame].w, move_right[frame].h, x, y, move_right[frame].w * 2, move_right[frame].h * 2)
+        y -= speed
+        if y < 40:
+            y += speed
 
 running = True
-h_dir = 0
+dir = 0
+
 frame = 0
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 speed = 10
@@ -84,7 +102,6 @@ while running:
     character_move()
     update_canvas()
     handle_events()
-
     frame = (frame + 1) % 8
     delay(0.05)
 
