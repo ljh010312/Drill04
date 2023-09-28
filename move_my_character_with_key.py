05a219f1b8f4f6a00b057c8627143c795fc5ee36
Lee9747 <leejunho32@naver.com>
2023-09-28 17:04:38 +0900

    Add esc key close button

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -8,7 +8,15 @@ character = load_image('spritesheets.png')
 
 
 def handle_events():
-   pass
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
 
 
 running = True
